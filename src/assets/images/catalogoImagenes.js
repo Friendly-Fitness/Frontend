f890e87bdf0d98e7afb2bbf97574c8abdbad6a18
paoloVD21 <pvizconded@hotmail.com>
2023-12-12 09:52:12 -0500

    Se Agrega los activos

---
+++ src/assets/images/catalogoImagenes.js
@@ -0,0 +1,21 @@
+import iniciarSesion from '../images/iniciarSesion.jpg';
+import logo from '../images/logo.jpg';
+import pagina from '../images/pagina.jpg';
+import perfil from '../images/perfil.jpeg';
+import presentacion from '../images/presentacion.jpg';
+import presentacion_2 from '../images/presentacion_2.webp';
+import registro from '../images/registro.jpeg';
+import tresPuntos from '../images/tresPuntos.jpg';
+import x from '../images/x.jpg';
+
+export default {
+  iniciarSesion,
+  logo,
+  pagina,
+  perfil,
+  presentacion,
+  presentacion_2,
+  registro,
+  tresPuntos,
+  x,
+};
---
+++ src/assets/videos/catalogoVideos.js
@@ -0,0 +1,51 @@
+import video1 from '../videos/Video1.mp4';
+import video2 from '../videos/Video2.mp4';
+import video3 from '../videos/Video3.mp4';
+import video4 from '../videos/Video4.mp4';
+import video5 from '../videos/Video5.mp4';
+import video6 from '../videos/Video6.mp4';
+import video7 from '../videos/Video7.mp4';
+import video8 from '../videos/Video8.mp4';
+import video9 from '../videos/Video9.mp4';
+import video10 from '../videos/Video10.mp4';
+import video11 from '../videos/Video11.mp4';
+import video12 from '../videos/Video12.mp4';
+import video13 from '../videos/Video13.mp4';
+import video14 from '../videos/Video14.mp4';
+import video15 from '../videos/Video15.mp4';
+import video16 from '../videos/Video16.mp4';
+import video17 from '../videos/Video17.mp4';
+import video18 from '../videos/Video18.mp4';
+import video19 from '../videos/Video19.mp4';
+import video20 from '../videos/Video20.mp4';
+import video21 from '../videos/Video21.mp4';
+import video22 from '../videos/Video22.mp4';
+import video23 from '../videos/Video23.mp4';
+import video24 from '../videos/Video24.mp4';
+
+export default {
+  video1,
+  video2,
+  video3,
+  video4,
+  video5,
+  video6,
+  video7,
+  video8,
+  video9,
+  video10,
+  video11,
+  video12,
+  video13,
+  video14,
+  video15,
+  video16,
+  video17,
+  video18,
+  video19,
+  video20,
+  video21,
+  video22,
+  video23,
+  video24,
+};
